71b1077aee7ea991a0a8c29a562f6573038070d4
Wekat <60980395+Wekat@users.noreply.github.com>
2020-04-08 10:51:58 +0200

    updated sheep count with better alternative

--- Homework2/script.js
+++ Homework2/script.js
@@ -22,16 +22,16 @@ The program solves it depending on the operation and shows the result. */
 /*2. Count the factorial of the number given by a user. 
 For example, 4! is 1 * 2 * 3 * 4 = 24, or 6! is 1 * 2 * 3 * 4 * 5 * 6 =  720.*/
 
-let num = +prompt("insert a number");
-let i = 1;
-let result = 1; 
+// let num = +prompt("insert a number");
+// let i = 1;
+// let result = 1; 
 
-while (i <= num) { 
-    result *= i;
-    i++;
-}
+// while (i <= num) { 
+//     result *= i;
+//     i++;
+// }
 
-console.log(result)
+// console.log(result)
 
 /* 3. Count sheep. Request a non-negative integer, 3 for example, and print a string with a murmur:
  "1 sheep...2 sheep...3 sheep..." to the page as a paragraph. If a user types a negative number in, 
@@ -46,6 +46,17 @@ console.log(result)
 // if (sheep <= 0) {
 //     alert("No sheep!")
 // }
+
+let sheep = prompt("How many sheep do you see?");
+
+if (sheep >=1) {
+    for (let i = 1; i <= sheep; i++) {
+        document.write(`<p>${i} sheep</p>`)
+    }
+} else {
+    alert("No sheep!")
+}
+
 
 
 /* 4. Show a user the prompt where they have to write a correct answer for 2 + 2 * 2. 
