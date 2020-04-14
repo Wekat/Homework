eacdb02f7cf2721bf150f79bc48e3f2bd185740c
Wekat <60980395+Wekat@users.noreply.github.com>
2020-04-14 09:05:20 +0200

    Update script.js

--- Homework3/script.js
+++ Homework3/script.js
@@ -15,22 +15,42 @@
 and returns a new string with all vowels removed. For example, “This website is for 
 losers LOL!” returns “Ths wbst s fr lsrs LL!“. */
 
+// function troll (string) {
+//     let newString = string;
+
+//     for (let i = 0; i <= string.length; i++) {
+//         if (string[i] == 'a') {
+//             newString = newString.replace(/a/ig,'');
+//         } else if (string[i] == 'e') {
+//             newString = newString.replace(/e/ig,'');
+//         } else if (string[i] == 'i') {
+//             newString = newString.replace(/i/ig,'');
+//         } else if (string[i] == 'o') {
+//             newString = newString.replace(/o/ig,'');
+//         } else if (string[i] == 'u') {
+//             newString = newString.replace(/u/ig,'');
+//         }
+//     }
+//     return newString;
+// }
+
+// console.log(troll('This website is for losers LOL!'));
+
+//else if not necessary
+
 function troll (string) {
     let newString = string;
 
-    for (let i = 0; i <= string.length; i++) {
-        if (string[i] == 'a') {
-            newString.replace('a','');
-        } else if (string[i] == 'e') {
-            newString.replace('e','');
-        } else if (string[i] == 'i') {
-            newString.replace('o','');
-        } else if (string[i] == 'o') {
-            newString.replace('o','');
-        } else if (string[i] == 'u') {
-            newString.replace('u','');
-        }
-    }
+    newString = newString.replace(/a/ig,'');
+
+    newString = newString.replace(/e/ig,'');
+
+    newString = newString.replace(/i/ig,'');
+
+    newString = newString.replace(/o/ig,'');
+
+    newString = newString.replace(/u/ig,'');
+        
     return newString;
 }
 
@@ -39,13 +59,22 @@ console.log(troll('This website is for losers LOL!'));
 /* Write a function which repeats the given string exact number of times. For example, 
 repeatString("Hello", 5) returns “HelloHelloHelloHelloHello”. */
 
+// Alternative with for loop but shorter alt below.
+
+// function repeatString (string, number) {
+//     let newString = string
+
+//     for (i = 1; i < number; i++) {
+//         newString = newString + string;
+//     }
+
+//     return newString;
+// }
+
+// console.log(repeatString("Hello", 5));
+
 function repeatString (string, number) {
-    let newString = string
-
-    for (i = 1; i < number; i++) {
-        newString + string == newString;
-    }
-
+    let newString = string.repeat(number);
     return newString;
 }
 
