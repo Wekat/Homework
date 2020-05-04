5ce427c69356c7412bf69de6f32c678738709922
Wekat <60980395+Wekat@users.noreply.github.com>
2020-05-04 14:38:43 +0200

    carousell assigment

---
+++ Homework8_jquery_slick/Assignment2_slick/script/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){ //why do I need this part. In your example you only used The inner funktion?
+    $('.mySlider').slick({
+      dots: true,
+      prevArrow: '<div class="slick-prev"></div>',
+      nextArrow: '<div class="slick-next"></div>'
+    });
+});
---
+++ Homework8_jquery_slick/Lesson8 javascript and html/script/main.js
@@ -0,0 +1,29 @@
+$( "#location" ).click(function() {
+    $( "#extended_location" ).slideToggle( "slow", function() {
+      // Animation complete.
+    });
+});
+
+$( "#music" ).click(function() {
+    $( "#extended_music" ).slideToggle( "slow", function() {
+      // Animation complete.
+    });
+});
+
+$( "#notes" ).click(function() {
+    $( "#extended_notes" ).slideToggle( "slow", function() {
+      // Animation complete.
+    });
+});
+
+$( "#books" ).click(function() {
+    $( "#extended_books" ).slideToggle( "slow", function() {
+      // Animation complete.
+    });
+});
+
+$( "#tendances" ).click(function() {
+    $( "#extended_tendances" ).slideToggle( "slow", function() {
+      // Animation complete.
+    });
+});
